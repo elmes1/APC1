25f83d5bec7086a3be0cbd53139c05d37a2b9cee
elmes1 <21653024-elmes1@users.noreply.replit.com>
2023-08-24 00:46:07 +0000

    arquivos da aula 1

---
+++ aulas/praticas/aula01/meuprograma.c
@@ -0,0 +1,17 @@
+/*
+primeiro programa
+*/
+
+
+
+#include <stdio.h>
+#include <math.h>
+//funcao principal
+
+int main ()
+{
+printf("ola pessoal!\n");
+printf("A raiz quadrada de 2 e %f\n", sqrt (2));
+
+  return 0;
+}
